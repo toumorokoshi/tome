f8233b8be0e64f6592a48adeea5aab0d2d9ac749
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2023-10-28 23:37:56 -0700

    fix: update version + deps (#50)
    Ensured that tome's version is printed properly. In addition,
    removed an erroneous echo statement.
    
    atty==0.2.14 seems to have a security vulnerability, so
    updated that.
    
    fixes #49

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ pub fn main() {
     let args: Vec<String> = args().peekable().collect();
     match execute(args) {
         Ok(result) => print!("{}", result),
-        Err(error_message) => print!("echo {}", error_message),
+        Err(error_message) => print!("{}", error_message),
     };
 }
